82fd63abe050b0b925a1cf38886b68950c0b2d0d
shilpi123-star <shilpi.bhavya2012@gmail.com>
2025-07-09 18:16:31 +0530

    demo for broken pipeline

--- src/test/java/api/cucumber/steps/PostSteps.java
+++ src/test/java/api/cucumber/steps/PostSteps.java
@@ -36,12 +36,12 @@ public class PostSteps {
    }
 */
 //    }
-    @Given("User should be logged in and should be present on his wall")
+    @("User should be logged in and should be present on his wall")
    public void user_should_be_logged_in_and_should_be_present_on_his_wall() {
        System.out.println("user_should_be_logged_in_and_should_be_present_on_his_wall");
         System.out.println("This is commit from feature into master");
      System.out.println("This demo is for git rebase from eclips");
-     System.ouuut.println("This is my first github commit integrating with jenkins");
+     System.out.println("This is my first github commit integrating with jenkins");
    }
   @When("I type the message in the box")
   public void i_type_the_message_in_the_box() {
